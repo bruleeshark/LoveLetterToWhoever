32b1d642ec8117f57b4a8463b47e898817a09611
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-02-13 18:00:23 -0700

    FINALLY COMPILES!!!

--- LoveLetterToWhoever.sol
+++ LoveLetterToWhoever.sol
@@ -1,16 +1,17 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
 
 contract LoveLetterToWhoever {
-    bytes32 public hash;
-    string public message;
-    address public owner;
-    uint256 public pin;
+    bytes32 private hash;
+    string private message;
+    address payable private owner;
+    address payable private _deployer;
+    uint256 private pin;
 
-    constructor() public {
-        owner = msg.sender;
-    }
+    constructor(address payable _deployer) public {
+        owner = _deployer;
+}
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action.");
@@ -28,6 +29,13 @@ contract LoveLetterToWhoever {
 
     function revealMessage(uint256 _pin) public view returns (string memory) {
         require(_pin == pin, "Incorrect PIN.");
+
         return message;
     }
+
+    function emergencyWithdraw() public onlyOwner payable {
+        require(address(this).balance > 0, "No funds to withdraw.");
+        owner.transfer(address(this).balance);
+    }
 }
+
